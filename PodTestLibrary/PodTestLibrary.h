05d652e2532a732c976ce71730fb40b3ed56fd62
zhiming liu <zhimingliu@zhimingdeiMac.local>
2018-01-24 17:43:52 +0800

    Initial Commit

---
+++ PodTestLibrary/PodTestLibrary.h
@@ -0,0 +1,19 @@
+//
+//  PodTestLibrary.h
+//  PodTestLibrary
+//
+//  Created by zhiming liu on 2018/1/24.
+//  Copyright © 2018年 zhiming liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodTestLibrary.
+FOUNDATION_EXPORT double PodTestLibraryVersionNumber;
+
+//! Project version string for PodTestLibrary.
+FOUNDATION_EXPORT const unsigned char PodTestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodTestLibrary/PublicHeader.h>
+
+
